52b61cbba4b32b7666a3665d52d2fa246264ff10
PFNK <43795837+PFNK@users.noreply.github.com>
2020-02-01 16:56:11 +0000

    Update read_csv.py

--- read_csv.py
+++ read_csv.py
@@ -2,10 +2,16 @@ import csv
 from query_db import query_db
 
 def read_query_output():
+    flight_id = ""
     with open('barcodes.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
-                print(row)
+                #print(row)
+                flight_id = row
                 line_count += 1
+         
+    details = query_db(flight_id[0])
+    #print(details)
+    return(flight_id)
