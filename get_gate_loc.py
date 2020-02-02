0f48c0c905d60bd51ddcdc2183f223c3b2b28364
zaceJin <35697340+zaceJin@users.noreply.github.com>
2020-02-02 18:40:52 +0000

    Update and rename connect_to_db.py to get_gate_loc.py

---
+++ get_gate_loc.py
@@ -0,0 +1,26 @@
+import mysql.connector
+#initiate the connection to the DB
+def gate_infor(gate_no):
+    conn = mysql.connector.connect(user='group6', password='turtlebot',
+                              host='localhost',
+                              database='main_sdp_db')
+
+    cursor = conn.cursor()
+# query string to be used for DB 
+    query = "SELECT * FROM gates WHERE gate_no = '{}'".format(gate_no) 
+    
+    output = []
+    cursor.execute(query)
+ 
+    row = cursor.fetchone()
+ 
+    while row is not None:
+        output.append(row)
+	      row = cursor.fetchone()
+    cursor.close()
+    conn.close()
+ 
+    return output[0]
+
+#if __name__ == '__main__':
+    #query_with_fetchone()
