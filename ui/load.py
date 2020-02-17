baf2b4d813b476ec5486f036ff57fd5e43a447a0
daraghmeehan <daraghmeehan@gmail.com>
2020-02-17 19:32:19 +0000

    Ready for test on Pi

--- ui/load.py
+++ ui/load.py
@@ -12,39 +12,41 @@ class MyWindow(QtWidgets.QStackedWidget, Ui_MainWindow):
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
 
-
-
         self.setCurrentWidget(self.START)
 
-        self.start.clicked.connect(lambda: self.StartScanning())
-        self.next.clicked.connect(lambda: self.setCurrentWidget(self.SUCCESS))
-        self.next2.clicked.connect(lambda: self.setCurrentWidget(self.INFO))
-        self.go_somewhere.clicked.connect(lambda: self.setCurrentWidget(self.WHERE))
 
-        self.gate.clicked.connect(lambda: self.ChooseLocation("Gate"))
-        self.toilet.clicked.connect(lambda: self.ChooseLocation("Toilets"))
-        self.shop.clicked.connect(lambda: self.setCurrentWidget(self.SHOPS))
+        self.start.clicked.connect(lambda: self.StartScanning())
+
+        # for testing
+        self.next_scan.clicked.connect(lambda: self.setCurrentWidget(self.SUCCESS))
+        self.next_success.clicked.connect(lambda: self.setCurrentWidget(self.INFO))
+
+        self.go_somewhere.clicked.connect(lambda: self.setCurrentWidget(self.WHERE))
+        self.back_to_info.clicked.connect(lambda: self.setCurrentWidget(self.INFO))
+
+        self.gate.clicked.connect(lambda: self.ChooseDestination("Gate"))
+        self.toilet.clicked.connect(lambda: self.ChooseDestination("Toilets"))
         self.food.clicked.connect(lambda: self.setCurrentWidget(self.FOOD))
-        self.whs_shop.clicked.connect(lambda: self.ChooseLocation("WHSmith"))
-        self.next_shop.clicked.connect(lambda: self.ChooseLocation("Next"))
-        self.mcd_food.clicked.connect(lambda: self.ChooseLocation("McDonalds"))
-        self.bburr_food.clicked.connect(lambda: self.ChooseLocation("Bar Burrito"))
+        self.shop.clicked.connect(lambda: self.setCurrentWidget(self.SHOPS))
+        self.mcd_food.clicked.connect(lambda: self.ChooseDestination("McDonalds"))
+        self.bburr_food.clicked.connect(lambda: self.ChooseDestination("Bar Burrito"))
+        self.whs_shop.clicked.connect(lambda: self.ChooseDestination("WHSmith"))
+        self.next_shop.clicked.connect(lambda: self.ChooseDestination("Next"))
 
         self.not_correct.clicked.connect(lambda: self.setCurrentWidget(self.WHERE))
+        self.yes_go.clicked.connect(lambda: self.setCurrentWidget(self.NAVIGATING))
 
-        self.yes_go.clicked.connect(lambda: self.setCurrentWidget(self.WHERE))
+        self.cancel_navigation.clicked.connect(lambda: self.setCurrentWidget(self.INFO))
+
 
     def StartScanning(self):
         self.setCurrentWidget(self.SCAN)
 
 
 
-
-
-
-    def ChooseLocation(self, location):
+    def ChooseDestination(self, location):
         self.setCurrentWidget(self.CONFIRM_DEST)
-        self.location.setText(location)
+        self.destination_label.setText(location + "?")
 
 
 
