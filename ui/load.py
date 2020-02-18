b30e91aa01b64ad99a009108cea818ab001bbd12
daraghmeehan <daraghmeehan@gmail.com>
2020-02-18 17:58:40 +0000

    Added navigate() to load.py

--- ui/load.py
+++ ui/load.py
@@ -35,9 +35,9 @@ class MyWindow(QtWidgets.QStackedWidget, Ui_MainWindow):
         self.back_to_info.clicked.connect(lambda: self.setCurrentWidget(self.INFO))
 
         # WHERE
-        self.gate.clicked.connect(lambda: self.chooseDestination("Gate " + self.cust.gate, self.INFO))
+        self.gate.clicked.connect(lambda: self.chooseDestination("Gate " + self.cust.gate, self.WHERE))
 
-        self.toilet.clicked.connect(lambda: self.chooseDestination("Toilets", self.INFO))
+        self.toilet.clicked.connect(lambda: self.chooseDestination("Toilets", self.WHERE))
 
         self.food.clicked.connect(lambda: self.setCurrentWidget(self.FOOD))
         # self.back_to_where_food.clicked.connect(lambda: self.setCurrentWidget(self.WHERE))
@@ -71,10 +71,17 @@ class MyWindow(QtWidgets.QStackedWidget, Ui_MainWindow):
         self.not_correct.clicked.connect(lambda: self.setCurrentWidget(self.WHERE))
         
         # NAVIGATION
-        self.cancel_navigation.clicked.connect(lambda: self.setCurrentWidget(self.INFO))
-        self.go_somewhere_else.clicked.connect(lambda: self.setCurrentWidget(self.WHERE))
-    
 
+        # PAUSE
+
+        # Help buttons
+        self.info_1.clicked.connect(lambda: self.setCurrentWidget(self.HELP))
+        self.info_2.clicked.connect(lambda: self.setCurrentWidget(self.HELP))
+        self.info_3.clicked.connect(lambda: self.setCurrentWidget(self.HELP))
+        self.info_4.clicked.connect(lambda: self.setCurrentWidget(self.HELP))
+        self.info_5.clicked.connect(lambda: self.setCurrentWidget(self.HELP))
+        self.info_6.clicked.connect(lambda: self.setCurrentWidget(self.HELP))
+        self.info_7.clicked.connect(lambda: self.setCurrentWidget(self.HELP))
 
 
     def scan(self):
@@ -103,10 +110,17 @@ class MyWindow(QtWidgets.QStackedWidget, Ui_MainWindow):
         self.setCurrentWidget(self.CONFIRM_DEST)
         self.destination_label.setText(location + "?")
         self.navigating_to_label2.setText(location)
-        self.yes_go.clicked.connect(lambda: self.setCurrentWidget(self.NAVIGATING))
+        self.navigating_to_label3.setText(location)
+        self.yes_go.clicked.connect(self.navigate(location))
         self.back_to_prev.clicked.connect(lambda: self.setCurrentWidget(previous_widget))
 
         # navigate here
+    
+    def navigate(self, location):
+
+        self.setCurrentWidget(self.NAVIGATING)
+        self.pause_navigation.clicked.connect(lambda: self.setCurrentWidget(self.PAUSE))
+        self.resume_navigation.clicked.connect(lambda: self.setCurrentWidget(self.NAVIGATING))
 
 
 if __name__ == "__main__":
