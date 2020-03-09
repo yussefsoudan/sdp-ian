310b02da1cb0500e182e13663413446c63f64cf0
Yussef Soudan <30552601+yussefsoudan@users.noreply.github.com>
2020-03-09 14:01:47 +0000

    Tiding up the script scanning the db

--- Live/third_pi/check_db.py
+++ Live/third_pi/check_db.py
@@ -7,13 +7,15 @@ import os
 conn = mysql.connector.connect(host = 'localhost',database = 'main_sdp_db',
 	user = 'root', password = 'turtlebot')	
 cursor = conn.cursor()
-query = "SELECT status FROM flights WHERE flight_id = '{}'".format(sys.argv[1])
+flight_id = sys.argv[1]
+query = "SELECT status FROM flights WHERE flight_id = '{}'".format(flight_id)
 cursor.execute(query)
 row = cursor.fetchone()
 output1 = row
+original_status = output1[0]
 cursor.close()
 conn.close()
-print(output1[0])
+print('Original status: ' + original_status)
 while True:
 	output2 = []
 	conn = mysql.connector.connect(host = 'localhost',database = 'main_sdp_db',
@@ -22,13 +24,14 @@ while True:
 	cursor.execute(query)
 	row = cursor.fetchone()
 	output2 = row
-	if output1[0] != output2[0]:
-		print('flight change')
-		#print(output2[0])
-		os.system ("ssh ubuntu@192.168.105.149 nohup python ~/Desktop/Demo2/sdp-ian/Live/change.py " + output2[0])
+	current_status = output2[0]
+	if original_status != current_status:
+		print('The flight status has changed!')
+		print('New status: ' + current_status)
+		os.system ("ssh ubuntu@192.168.105.149 nohup python ~/Desktop/Demo2/sdp-ian/Live/second_pichange.py " + current_status)
 		break
 	else: 
-		print('flight not change')
+		print('No status change...')
 	cursor.close()
 	conn.close()
 	time.sleep(5)
