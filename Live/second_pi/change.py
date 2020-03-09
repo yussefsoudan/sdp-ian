ddc79e1ddf43ad73e0714fd51c0e854c85d0a122
Yussef Soudan <30552601+yussefsoudan@users.noreply.github.com>
2020-03-09 16:23:32 +0000

    Added departure time

--- Live/second_pi/change.py
+++ Live/second_pi/change.py
@@ -1,3 +1,4 @@
 import sys
-new_status = str(sys.argv[1:])
-print('You flight has changed:' + ' ' + new_status)
+new_status = str(sys.argv[1])
+new_depart_time = str(sys.argv[2])
+print('You flight has changed:' + ' ' + new_status + ' ' + new_depart_time)
