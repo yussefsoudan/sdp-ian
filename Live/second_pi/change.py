5a1012b632a57b503fa93c98885bd79166b01153
Yussef Soudan <30552601+yussefsoudan@users.noreply.github.com>
2020-03-10 18:30:22 +0000

    Debugging live updates

--- Live/second_pi/change.py
+++ Live/second_pi/change.py
@@ -1,6 +1,12 @@
 import sys
+
 new_status = str(sys.argv[1])
 new_depart_time = str(sys.argv[2])
-updateFile = '../../ui/update.txt'
-with open(updateFile, 'w') as filetowrite:
-    filetowrite.write('You flight has changed:' + '\n' + new_status + '\n' + new_depart_time)
+
+updateFile = './sdp-ian/ui/update.txt'
+f = open(updateFile, "w+")
+
+f.write('You flight has changed:' + '\n' + new_status + '\n ' + new_depart_time)
+
+f.close()
+
--- Live/second_pi/check_for_status_change.py
+++ Live/second_pi/check_for_status_change.py
@@ -1,4 +1,4 @@
 import os
-
+import sys
 flight_id = sys.argv[1]
-os.system ("ssh ubuntu@192.168.105.28 nohup python ~/Desktop/check_db.py " + flight_id)
+os.system ("ssh ubuntu@192.168.105.28 nohup python ~/sdp-ian/Live/third_pi/check_db.py " + flight_id)
