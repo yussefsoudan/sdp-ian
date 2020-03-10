7f9753abfb6af078a63e69f4d3027962d409adeb
Yussef Soudan <30552601+yussefsoudan@users.noreply.github.com>
2020-03-10 19:45:53 +0000

    Debugging live updates

--- Live/third_pi/check_db.py
+++ Live/third_pi/check_db.py
@@ -12,13 +12,7 @@ query = "SELECT status FROM flights WHERE flight_id = '{}'".format(flight_id)
 cursor.execute(query)
 row = cursor.fetchone()
 output1 = row
-original_status = output1[0]
-
-query = "SELECT depature_time FROM flights WHERE flight_id = '{}'".format(flight_id)
-cursor.execute(query)
-row = cursor.fetchone()
-output3 = row
-new_depart_time = str(output3[0])
+original_status = str(output1[0])
 
 cursor.close()
 conn.close()
@@ -28,14 +22,22 @@ while True:
 	conn = mysql.connector.connect(host = 'localhost',database = 'main_sdp_db',
 	user = 'root', password = 'turtlebot')
 	cursor = conn.cursor()	
-	cursor.execute(query)
+	cursor.execute("SELECT status_after FROM flight_changes WHERE flight_id = '{}'".format(flight_id))
 	row = cursor.fetchone()
 	output2 = row
-	current_status = str(output2[0])
-	if original_status != current_status:
+	
+	if row is not None:	
+		current_status = str(output2[0])
 		print('The flight status has changed!')
 		print('New status: ' + current_status)
-		os.system ("ssh ubuntu@192.168.105.149 nohup python ~/sdp-ian/Live/second_pi/change.py " + 'delayed' + " " + new_depart_time)
+		
+		query = "SELECT depature_time FROM flights WHERE flight_id = '{}'".format(flight_id)
+		cursor.execute(query)
+		row = cursor.fetchone()
+		output3 = row
+		new_depart_time = str(output3[0])
+		
+		os.system ("ssh ubuntu@192.168.105.149 nohup python ~/sdp-ian/Live/second_pi/change.py " + current_status + " " + new_depart_time)
 		break
 	else: 
 		print('No status change...')
