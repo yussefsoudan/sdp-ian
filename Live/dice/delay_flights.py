df1e64ce24591d06388957a384c27e3a8095ca6b
Yussef Soudan <30552601+yussefsoudan@users.noreply.github.com>
2020-03-12 14:58:49 +0000

    Debugging delay-flights script

--- Live/dice/delay_flights.py
+++ Live/dice/delay_flights.py
@@ -7,8 +7,8 @@ cursor = conn.cursor()
 
 # These queries delay all flights by 20 minutes.
 status_query = "UPDATE flights SET status = 'Delayed'"
-boarding_time_query = "UPDATE flights SET boarding_time = ADDTIME(date_col, '0:20:0')"
-depature_time_query = "UPDATE flights SET depature_time = ADDTIME(date_col, '0:20:0')"
+boarding_time_query = "UPDATE flights SET boarding_time = ADDTIME(boarding_time, '0:20:0')"
+depature_time_query = "UPDATE flights SET depature_time = ADDTIME(depature_time, '0:20:0')"
 
 # The final query, the status_query, will automatically update the flight_changes table,
 # which will then cause the third_pi/check_db.py script to alert the user.
