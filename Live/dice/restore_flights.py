60547393d1197d9a13554e7567b2294a48fac096
Yussef Soudan <30552601+yussefsoudan@users.noreply.github.com>
2020-03-13 13:32:09 +0000

    Debugging restore_flights.py

--- Live/dice/restore_flights.py
+++ Live/dice/restore_flights.py
@@ -3,7 +3,7 @@ import mysql.connector
 connection = mysql.connector.connect(host='192.168.105.28', 
 database='main_sdp_db', user='secondPi', password='turtlebot', port=3306)
 
-cursor = conn.cursor()
+cursor = connection.cursor()
 
 # These queries bring all flights back by 20 minutes and the status 'On time'.
 status_query = "UPDATE flights SET status = 'On time'"
@@ -13,11 +13,11 @@ depature_time_query = "UPDATE flights SET depature_time = SUBTIME(depature_time,
 # The final query, the status_query, will automatically update the flight_changes table,
 # which will then cause the third_pi/check_db.py script to alert the user.
 cursor.execute(boarding_time_query)
-cursor.commit()
+connection.commit()
 cursor.execute(depature_time_query)
-cursor.commit()
+connection.commit()
 cursor.execute(status_query)
-cursor.commit()
+connection.commit()
 
 cursor.close()
 connection.close()
