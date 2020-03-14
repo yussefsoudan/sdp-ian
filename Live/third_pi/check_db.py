f5a6f6f0b3d82145c2294650603ae95f42d46318
Yussef Soudan <30552601+yussefsoudan@users.noreply.github.com>
2020-03-14 18:21:03 +0000

    Debugging

--- Live/third_pi/check_db.py
+++ Live/third_pi/check_db.py
@@ -25,22 +25,29 @@ while True:
 	cursor.execute("SELECT status_after FROM flight_changes WHERE flight_id = '{}'".format(flight_id))
 	row = cursor.fetchone()
 	output2 = row
-	
+	cursor.close()
+	conn.close()
 	if row is not None:	
 		current_status = str(output2[0])
 		print('The flight status has changed!')
 		print('New status: ' + current_status)
 		
+		output3 = []
+		conn = mysql.connector.connect(host = 'localhost',database = 'main_sdp_db',
+		user = 'root', password = 'turtlebot')
+		cursor = conn.cursor()
 		query = "SELECT depature_time FROM flights WHERE flight_id = '{}'".format(flight_id)
 		cursor.execute(query)
+		
 		row = cursor.fetchone()
 		output3 = row
 		new_depart_time = str(output3[0])
 		
 		os.system ("ssh ubuntu@192.168.105.149 nohup python ~/sdp-ian/Live/second_pi/change.py " + current_status + " " + new_depart_time)
+		cursor.close()
+		conn.close()
 		break
 	else: 
 		print('No status change...')
-	cursor.close()
-	conn.close()
+	
 	time.sleep(5)
