b48cc61efad13e879ea4e267de167c0930cad023
Yussef Soudan <30552601+yussefsoudan@users.noreply.github.com>
2020-03-30 15:30:43 +0100

    Update get_gate_no.py

--- Scanning/get_gate_no.py
+++ Scanning/get_gate_no.py
@@ -1,9 +1,12 @@
 import mysql.connector
+import sys
+sys.path.insert(0, '~/sdp-ian/env.py')
+import env
 
 #initiate the connection to the DB
 def gate_no(name):
 	conn = mysql.connector.connect(user='secondPi', password='turtlebot',
-                              host='192.168.105.28',
+                              host=SERVER_PI_IP,
                               database='main_sdp_db',port =3306)
 	cursor = conn.cursor()
 # query string to be used for DB 
