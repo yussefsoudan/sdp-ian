a1ef491eeb3e681798015349a45354f3f9e666d1
Yussef Soudan <30552601+yussefsoudan@users.noreply.github.com>
2020-03-30 15:27:23 +0100

    Update check_for_status_change.py

--- Live/second_pi/check_for_status_change.py
+++ Live/second_pi/check_for_status_change.py
@@ -1,4 +1,7 @@
 import os
 import sys
+sys.path.insert(0, '~/sdp-ian/env.py')
+import env
+
 flight_id = sys.argv[1]
-os.system ("ssh ubuntu@192.168.105.28 nohup python ~/sdp-ian/Live/third_pi/check_db.py " + flight_id)
+os.system ("ssh ubuntu@" + SERVER_PI_IP + "nohup python ~/sdp-ian/Live/third_pi/check_db.py " + flight_id)
